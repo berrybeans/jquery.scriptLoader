cd43f5c9751f9cb387eacd9d95be48f96bb59602
berrybeans <daa0006@auburn.edu>
2015-07-16 09:33:52 -0500

    Update script_load.js
    made the plugin much more robust and actually extended jquery

--- script_load.js
+++ script_load.js
@@ -1,64 +1,62 @@
-$.script_load = function (obj) {
-    /*obj fields:
-     *  script_loc:     string,
-     *  script_lib:     [{path: string}, ...],
-     *  timeout:        int,
-     *  debug:          bool,
-     *  alertify:       bool,
-     *  callback:      function
-     */
+(function ($) {
+    $.script_load = function (obj) {
+        //create a local object
+        var _obj = {
+            //[PARAM]       : [NULL TEST]       ? [DEFAULT VALUE]   : [SUPPLIED VALUE]
+            script_lib      : obj.script_lib    ? obj.script_lib    : [],
+            timeout         : obj.timeout       ? obj.timeout       : 50,
+            alertify        : obj.alertify      ? obj.alertify      : false,
+            debug           : obj.debug         ? obj.debug         : false,
+            callback        : obj.callback      ? obj.callback      : function () { },
+            script_loc      : obj.script_loc    ? obj.script_loc    : '',
+            success_msg     : obj.success_msg   ? obj.success_msg   : "Scripts loaded.",
+            fail_msg        : obj.fail_msg      ? obj.fail_msg      : "Scripts failed to load."
+        };
 
-    obj.timeout     =   (obj.timeout === undefined)         ? 50            : obj.timeout;
-    obj.alertify    =   (obj.alertify === undefined)        ? true          : obj.alertify;
-    obj.debug       =   (obj.debug === undefined)           ? false         : obj.debug;
-    obj.callback    =   (obj.callback === undefined)        ? function () {}: obj.callback;
-    obj.script_loc  =   (obj.script_loc === undefined)      ? ''            : obj.script_loc;
+        var error = false;
+        var alertify_enabled = (alertify != undefined) && _obj.alertify;
 
-    var error = false;
-    var alertify_enabled = (alertify != undefined) && obj.alertify;
-
-    $.each(obj.script_lib, function (i, d) {
-        $.getScript(obj.script_loc + d.path, function (data, textStatus, jqxhr) {
-        }).success(function () {
-            if (obj.debug && alertify_enabled) {
-                alertify.log("Loaded " + d.path + ".");
-            }
-            d.loaded = true;
-        }).fail(function () {
-            if (obj.debug && alertify_enabled) {
-                alertify.error("Failed to load " + d.path + ".");
-            }
-            error = true;
+        $.each(_obj.script_lib, function (i, d) {
+            $.getScript(_obj.script_loc + d.path, function (data, textStatus, jqxhr) {
+            }).success(function () {
+                if (_obj.debug && alertify_enabled)
+                    alertify.log("Loaded " + d.path + ".");
+                d.loaded = true;
+            }).fail(function () {
+                if (_obj.debug && alertify_enabled)
+                    alertify.error("Failed to load " + d.path + ".");
+                error = true;
+            });
         });
-    });
 
-    var poll = function () {
-        setTimeout(function () {
-            obj.timeout--;
-            if (obj.script_lib.map(function (d) { return d.loaded; }).indexOf(false) === -1 && !error) {
-                try {
-                    obj.callback();
-                    if (alertify_enabled)
-                        alertify.success("Scripts loaded.");
-                } catch (e) {
-                    if (obj.debug) {
+        var poll = function () {
+            setTimeout(function () {
+                _obj.timeout--;
+                if (_obj.script_lib.map(function (d) { return d.loaded; }).indexOf(false) === -1 && !error) {
+                    try {
+                        _obj.callback();
                         if (alertify_enabled)
-                            alertify.error('CRITICAL ERROR WHILE LOADING: ' + e);
-                        throw e;
-                    } else {
-                        poll();
+                            alertify.success(_obj.success_msg);
+                    } catch (e) {
+                        if (_obj.debug) {
+                            if (alertify_enabled)
+                                alertify.error('CRITICAL ERROR WHILE LOADING: ' + e);
+                            throw e;
+                        } else {
+                            poll();
+                        }
                     }
-                }
 
-            } else if (obj.timeout > 0) {
-                poll();
-            } else {
-                if (alertify_enabled)
-                    alertify.error("Scripts failed to load.");
-                else
-                    alert("Scripts failed to load.");
-            }
-        }, 100);
-    };
-    poll();
-}
+                } else if (_obj.timeout > 0) {
+                    poll();
+                } else {
+                    if (alertify_enabled)
+                        alertify.error(_obj.fail_msg);
+                    else
+                        alert("Scripts failed to load.");
+                }
+            }, 100);
+        };
+        poll();
+    }
+}(jQuery))
